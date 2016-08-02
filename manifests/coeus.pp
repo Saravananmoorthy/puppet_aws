1b9e43467e1835d2283af807d886eb9ad85785fb
Khalid Khan <knkhan@gmail.com>
2016-08-02 10:56:12 +0500

    rds db creation code added to coeus.pp

--- manifests/coeus.pp
+++ manifests/coeus.pp
@@ -1,3 +1,4 @@
+
 ec2_instance { 'puppet-test':
   ensure                   => 'present',
   availability_zone        => 'eu-central-1a',
@@ -7,5 +8,25 @@ ec2_instance { 'puppet-test':
   region                   => 'eu-central-1',
   security_groups          => ['puppet-security'],
   subnet		   => 'subnet-default',
+  
 }
+
+		
+
+rds_instance { 'puppetdb':
+  ensure	=> 'present',
+  db_name        => 'coeusdb',
+  db_instance_class => 'db.t2.small',
+  region         => 'eu-central-1',
+  engine         => 'mysql',
+  engine_version => '5.5.40b',
+  allocated_storage => '5',
+  storage_type  => 'gp2',
+  master_username  => 'admin',
+  master_user_password => 'coeus123!',
+  multi_az  => 'false',
+                        
+} 
+
+
 
